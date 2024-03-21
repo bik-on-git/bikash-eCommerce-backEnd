30eb5187d08467c3a2f42fafdb87ec18dcc4b81f
bik-on-git <bikash.me.shrestha@gmail.com>
2024-03-21 10:04:12 -0700

    initial project update

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.frontend;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrontendApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrontendApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
